02c67b0c866df9e5dd9543c39c862b0217f88c27
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-10-14 22:08:50 +0530

    feat : using readable stores

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,9 +1,19 @@
 <script>
-    import settings from "../stores/setting.js";
-
+  import settings from "../stores/setting.js";
+  import location from "../stores/location.js";
 </script>
 
 <div>
-    <h1>{$settings.colorScheme}</h1>
-    <h2>Home Page</h2>
+  {#if $location && !$location.error}
+    <p>Latitude : {$location?.latitude} - Longitude : {$location?.longitude}</p>
+  {/if}
+
+  {#if $location && $location.error}
+    <p>{$location.error}</p>
+  {/if}
+</div>
+
+<div>
+  <h1>{$settings.colorScheme}</h1>
+  <h2>Home Page</h2>
 </div>
